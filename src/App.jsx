244b79949b6074a0374a9ccf89f898d337182529
Dainis Dilevka <thedenisovan@proton.me>
2025-09-20 13:46:03 +0100

    Add: api call effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import Header from './components/Header';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [flagUrl, setFlagUrl] = useState(
+    'https://api.api-ninjas.com/v1/countryflag?country=US'
+  );
+
+  // Fetch flag with given country code and set it inside current flag url state
+  useEffect(() => {
+    async function fetchFlag() {
+      try {
+        const response = await fetch(
+          'https://api.api-ninjas.com/v1/countryflag?country=US',
+          {
+            headers: {
+              'X-Api-Key': 's6x+NPhLcnIrBf/3qO5U7w==ojs2CSSWu64nj56J',
+            },
+          }
+        );
+        if (!response.ok) throw new Error('Failed to fetch flag');
+        const result = await response.json();
+        setFlagUrl(result.rectangle_image_url);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchFlag();
+  }, [flagUrl]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Header />
+      <img className='p-10' src={flagUrl} />
+    </div>
+  );
 }
 
-export default App
+export default App;
